6336875bf2b81b4957686546290e2fcd03395d44
Artem <tema.693168@gmail.com>
2021-04-19 12:39:04 +0400

    fix: task 3.3.3

--- Task 3/Task 3.3/Task 3.3.3/Program.cs
+++ Task 3/Task 3.3/Task 3.3.3/Program.cs
@@ -11,65 +11,69 @@ namespace Task_3._3._3
             var pizzeria = new Pizzeria();
 
             var customer = new Customer("Alex"); 
-            customer.AddToOrder(0);
-            customer.AddToOrder(2, 2);
-            customer.CompleteOrder(pizzeria);
+            var order1 = new Order(customer.Name, pizzeria);
+
+            customer.AddToOrder(order1, 0);
+            customer.AddToOrder(order1, 2, 2);
+            customer.CompleteOrder(order1);
 
             var customer1 = new Customer("Artem"); 
-            customer1.AddToOrder(1);
-            customer1.CompleteOrder(pizzeria);
+            var order2 = new Order(customer1.Name, pizzeria);
+            customer1.AddToOrder(order2,1);
+            customer1.CompleteOrder(order2);
 
             pizzeria.StartCooking();
         }
     }
 
     class Order{
-        public event Action<Order> OnReady;
+        private Pizzeria Pizzeria{get; set;}
         public string CustumerName{get; private set;}
         public Dictionary<int, int> PizzaIdCount;
-        public Order(string custumerName){
+        public Order(string custumerName, Pizzeria pizzeria){
             PizzaIdCount = new Dictionary<int, int>();
             CustumerName = custumerName;
+            Pizzeria = pizzeria;
         }
         public void AddToOrder(int pizzaId, int count = 1){
-            if(count > 0){
+            if(count < 0){
+                throw new ArgumentException(); 
+            }
+            else {
                 if(PizzaIdCount.ContainsKey(pizzaId)) PizzaIdCount[pizzaId] += count;
                 else PizzaIdCount.Add(pizzaId, count);
-            }
-            else throw new ArgumentException();
+            } 
         }
         public void RemoveFromOrder(int pizzaId, int count = 1){
-            if(count > 0){
+            if(count < 0){
+                throw new ArgumentException();
+            }
+            else {
                 if(PizzaIdCount.ContainsKey(pizzaId) && PizzaIdCount[pizzaId] - count > 1) PizzaIdCount[pizzaId] -= count;
                 else PizzaIdCount.Remove(pizzaId);
-            }
-            else throw new ArgumentException();
+            };
         }
-        public void Ready(){
-            OnReady?.Invoke(this);
+        public void CompleteOrder(){
+            Pizzeria.AddOrder(this);
         }
     }
 
     class Pizza{
-        private int _CookingTime{get; set;}
+        public int CookingTime{get; private set;}
         public string Name{get; private set;}
         public double Price{get; private set;}
-        public event Action<Pizza> OnCoocked;
         public Pizza(string name, double price, int cookingTime){
             Name = name;
             Price = price;
-            _CookingTime = cookingTime;
-        }
-
-        public void Cook(){
-            Thread.Sleep(_CookingTime);
-            Console.WriteLine($"Пицца {Name} готова");
-            OnCoocked?.Invoke(this);
+            CookingTime = cookingTime;
         }
     }
 
     class Pizzeria{
         public string[] MenuList{get; private set;}
+        public Pizza[] PizzasList{get; private set;}
+        public event Action<Pizza> OnCoocked;
+        public event Action<Order> OnOrderIsReady;
         private List<Pizza> PizzasForCustumer{get; set;}
         private List<Order> OrdersList{get; set;}
         public Pizzeria(){
@@ -78,41 +82,34 @@ namespace Task_3._3._3
                 "№2 Ветчина и сыр",
                 "№3 Пепперони"
             };
+            PizzasList = new Pizza[]{
+                new Pizza("Сырная", 255, 1000),
+                new Pizza("Ветчина и сыр", 270, 1600),
+                new Pizza("Пепперони", 230, 900)
+            };
             OrdersList = new List<Order>();
             PizzasForCustumer = new List<Pizza>();
+            OnCoocked += PizzaIsReady;
+            OnOrderIsReady += OrderIsReady;
         }
 
         public void AddOrder(Order order){
-            order.OnReady += OrderIsReady;
             OrdersList.Add(order);
         }
 
         public void StartCooking(){
             foreach(Order order in OrdersList){
                 foreach(KeyValuePair<int, int> keyValue in order.PizzaIdCount){
-                    switch (keyValue.Key){
-                        case 0: {
-                            CookPizza(new Pizza("Сырная", 255, 1000), keyValue.Value);
-                            break;
-                        }
-                        case 1: {
-                            CookPizza(new Pizza("Ветчина и сыр", 270, 1600), keyValue.Value);
-                            break;
-                        }
-                        case 2: {
-                            CookPizza(new Pizza("Пепперони", 230, 900), keyValue.Value);
-                            break;
-                        }
-                    }
+                    CookPizza(PizzasList[keyValue.Key], keyValue.Value);
                 }
-                order.Ready();
+                OnOrderIsReady?.Invoke(order);
             }
         }
 
         public void CookPizza(Pizza pizza, int count){
             for(int i = 0; i < count; i++){
-                pizza.OnCoocked += PizzaIsReady;
-                pizza.Cook();
+                Thread.Sleep(pizza.CookingTime);
+                OnCoocked?.Invoke(pizza);
             }
         }
 
@@ -121,26 +118,28 @@ namespace Task_3._3._3
         }
 
         public void OrderIsReady(Order order){
-            Console.WriteLine($"Заказ для {order.CustumerName} готов.");
+            Console.WriteLine($"Заказ для {order.CustumerName} готов:");
+            for(int i = 0; i < PizzasForCustumer.Count; i++){
+                Console.Write(PizzasForCustumer[i].Name + " ");
+            }
+            Console.WriteLine();
             PizzasForCustumer.Clear();
         }
     }
 
     class Customer{
         public string Name{get; private set;}
-        public Order CustomerOrder{get; private set;}
         public Customer(string name){
             Name = name;
-            CustomerOrder = new Order(Name);
         }
-        public void AddToOrder(int pizzaId, int count = 1){
-            CustomerOrder.AddToOrder(pizzaId, count);
+        public void AddToOrder(Order order, int pizzaId, int count = 1){
+            order.AddToOrder(pizzaId, count);
         }
-        public void RemoveFromOrder(int pizzaId, int count = 1){
-            CustomerOrder.RemoveFromOrder(pizzaId, count);
+        public void RemoveFromOrder(Order order, int pizzaId, int count = 1){
+            order.RemoveFromOrder(pizzaId, count);
         }
-        public void CompleteOrder(Pizzeria pizzeria){
-            pizzeria.AddOrder(CustomerOrder);
+        public void CompleteOrder(Order order){
+            order.CompleteOrder();
         }
     }
 }
